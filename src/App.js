29c38744dfe7ea2571fa9b8258954aa5c2d34b24
Oladepo Olabayoji <bayoji7@gmail.com>
2022-01-07 22:51:22 +0100

    change title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./container/Layout/Header";
 import Meals from "./container/Meals/Meals";
 import Cart from "./container/Cart/Cart";
 import CartProvider from "./Store/CartProvider";
 function App() {
+  // This effect runs once, after the first render
+  useEffect(() => {
+    document.title = "My Restaurant";
+  }, []);
   const [showCart, setShowCart] = useState(false);
 
   const showCartHandler = () => {
